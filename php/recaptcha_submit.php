a97a9b48445022f6df4409df66b8321fa4eae50f
codexfocus <codexfocus@users.noreply.github.com>
2017-04-25 10:44:30 -0600

    Create recaptcha_submit.php

---
+++ php/recaptcha_submit.php
@@ -0,0 +1,26 @@
+<?php
+
+$captcha;
+$secret = "SECERT_KEY_GOES_HERE";
+
+if(isset($_POST['g-recaptcha-response']))
+{
+  $captcha = $_POST['g-recaptcha-response'];
+}
+
+if(!$captcha)
+{
+  //captcha was not sent return back to the form
+} else {
+
+  $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+  
+  if($response.success==false)
+  {
+    //response was false return to the form
+  } else {
+    //proceed with processing your form
+  }
+}
+
+?>
